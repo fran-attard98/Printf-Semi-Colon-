8fa4cda22fe5005e66d485bc042ddf45a950e359
Matthias <matthias.attard.9@gmail.com>
2018-12-14 16:19:01 +0100

    Created simple calculator

---
+++ cmake-build-debug/CMakeFiles/Calc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calc.dir/main.c.obj"
+  "Calc.pdb"
+  "Calc.exe"
+  "Calc.exe.manifest"
+  "libCalc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
